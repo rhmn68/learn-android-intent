1a92242510a84c435aae6c3565fc0d43b87ba930
rhmn68 <auliar68@gmail.com>
2019-11-19 06:40:42 +0700

    first init

---
+++ app/src/main/java/com/rahmanaulia/learnintent/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.rahmanaulia.learnintent
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        onClick()
+    }
+
+    private fun onClick() {
+        btnMoveActivity.setOnClickListener {
+
+        }
+
+        btnMoveWithData.setOnClickListener {
+
+        }
+
+        btnMoveWithObject.setOnClickListener {
+
+        }
+
+        btnDialNumber.setOnClickListener {
+
+        }
+
+        btnMoveActivityWithResult.setOnClickListener {
+            
+        }
+    }
+}
